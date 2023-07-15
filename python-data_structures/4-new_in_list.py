76579d0e3eb60e164d88b7e501bc864cc7224d39
Komaiya Samuel <s.komaiya@alustudent.com>
2023-07-15 04:19:14 -0700

    task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         new_list = my_list.copy
-        new_list[2] = 4
+        new_list[2] = [4]
         return new_list
     else:
         return my_list
