84acef0046ab0385d7f0d8873c01e643cec3ce46
Komaiya Samuel <s.komaiya@alustudent.com>
2023-07-17 07:48:04 -0700

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
