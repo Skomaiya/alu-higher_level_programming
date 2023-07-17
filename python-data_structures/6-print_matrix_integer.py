cfcea681c23466e97443c282153e7f1a7eac8143
Komaiya Samuel <s.komaiya@alustudent.com>
2023-07-17 03:26:26 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-for i in matrix:
-    print("The matrix is " {:d}.format(i))
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
