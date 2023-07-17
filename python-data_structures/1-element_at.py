544916b49b6e5153775baaf817182aa7fc537814
Komaiya Samuel <s.komaiya@alustudent.com>
2023-07-17 03:05:14 -0700

    task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0:
-        return "none"
-    for num in my_list:
-        if idx > num:
-            return "none"
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
