758301e38994df698fe3709d9a2f89c8ecfc04e5
Komaiya Samuel <s.komaiya@alustudent.com>
2023-08-12 01:26:12 -0700

    task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -19,4 +19,4 @@ class City(Base):
 
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullableFalse)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
