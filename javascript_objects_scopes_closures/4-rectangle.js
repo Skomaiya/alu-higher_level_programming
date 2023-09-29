e46af009b92bbdc493e7cddbb26deae672109fe0
Skomaiya <s.komaiya@alustudent.com>
2023-09-29 00:39:30 -0700

    task 4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -3,22 +3,16 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-  print() {
-    if (Object.keys(this).length === 0) {
-      console.log('Empty object');
-    } else {
-      const row = 'X'.repeat(this.width);
-      for (let i = 0; i < this.height; i++) {
-        console.log(row);
-      }
-    }
+
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
-  rotate() {
-    this.width, this.height = this.height, this.width
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
   }
-  
-  double() {
-    this.width, this.height = this.width *= 2,  this.height *= 2
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
