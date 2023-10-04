5a2245ee745d3ef16182ef163dbd55382b0df2af
Skomaiya <s.komaiya@alustudent.com>
2023-10-04 12:09:21 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-
-const readAndPrintFile = (filePath) => {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(`An error occurred: ${err.message}`);
-    } else {
-      console.log(data);
-    }
-  });
-};
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node readFile.js <file_path>');
-} else {
-  const filePath = process.argv[2];
-  readAndPrintFile(filePath);
-}
+const FileName = process.argv[2];
+fs.readFile(FileName, 'utf8', function (err, line) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(line);
+  }
+});
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: node readFile.js <file_path>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-  fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-    } else {
-      console.log('Content written to file successfully.');
-    }
-  });
+const FileName = process.argv[2];
+fs.readFile(FileName, 'utf8', function (err, line) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(line);
+  }
+});
