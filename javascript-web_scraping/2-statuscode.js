fe342dea5234f5dfde108322ebf468de1da480f0
Skomaiya <s.komaiya@alustudent.com>
2023-10-04 12:15:20 -0700

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
+
 const request = require('request');
-
-if (process.argv.length !== 3) {
-    console.error('Usage: node readFile.js <file_path>');
-    process.exit(1);
-}
-
 const url = process.argv[2];
-
-request.get(url, (error, response) => {
-    if (error) {
-        console.error('Error:', error.message);
-      } else {
-        console.log('code: ', response.statusCode);
-      }
-    });
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
+  });
