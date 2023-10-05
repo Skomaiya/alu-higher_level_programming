f37b6cfa9c41bcb838040bc6e5e2616096d8618e
Skomaiya <s.komaiya@alustudent.com>
2023-10-05 02:56:32 -0700

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 3-starwars_title.js <movie-ID>');
-  process.exit(1);
-}
-
-const movieID = process.argv[2];
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-
-request.get(apiUrl, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
-    }, 0));
-  }
-});
