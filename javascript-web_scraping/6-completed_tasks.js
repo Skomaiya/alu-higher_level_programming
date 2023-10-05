55c2b9d0674632c6d269e886cd68f889fa0c685c
Skomaiya <s.komaiya@alustudent.com>
2023-10-05 03:11:29 -0700

    task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,32 @@
 #!/usr/bin/node
+const req = require('request');
 
-const request = require('request');
+function getCompletedTasks (data, userId) {
+  let count = 0;
+  data
+    .filter((element) => element.userId === userId)
+    .forEach((task) => {
+      if (task.completed) {
+        count++;
+      }
+    });
+  return count;
+}
+
 const url = process.argv[2];
-request.get(url, function (err, response, body) {
+
+const results = {};
+req.get(url, (err, res) => {
   if (err) {
-    console.log(err);
-  } else {
-    const todos = JSON.parse(body);
+    throw err;
+  }
+  const data = JSON.parse(res.body);
+  data.forEach((element) => {
+    if (!(element.userId in results)) {
+      if (getCompletedTasks(data, element.userId) > 0) {
+        results[element.userId] = getCompletedTasks(data, element.userId);
+      }
+    }
+  });
+  console.log(results);
+});
